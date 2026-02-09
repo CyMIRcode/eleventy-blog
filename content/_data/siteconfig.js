991e1fb49b8e78e9585ba104f39eccdc74d67ec7
chris <148228792+CyMIRcode@users.noreply.github.com>
2026-02-09 20:40:54 +0800

    Update site title in siteconfig.js

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     // Website title, shown in left sidebar and in page title
-    title: "旧梦|Yesterday\'s Tomorrow",
+    title: "旧梦",
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
